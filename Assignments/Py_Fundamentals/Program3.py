539f7fa237769a18c846a1b28ff9b56021829a7d
Rushikesh Vilas chavhan <rushikeshchavhan23@gmail.com>
2026-01-23 02:56:29 +0530

    Delete Assignments/Py_Fundamentals/Program3.py

--- Assignments/Py_Fundamentals/Program3.py
+++
@@ -1,9 +0,0 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-
-future_age = age + 13
-
-print("Hello ", name, "you will turn", future_age, " years old after 13 years.")
-
-
-# print(f"Hello {name}, you will turn {future_age} years old after 13 years.")
